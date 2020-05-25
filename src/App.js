f8375cbb41da8d6bd97ffb79b2e50fea0d0556e3
Juanrii <55418564+Juanrii@users.noreply.github.com>
2020-05-25 19:45:53 -0300

    Upload project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../src/components/Layout/Layout';
+import BurgerBuilder from '../src/containers/BurgerBuilder/BurgerBuilder';
+
+function App() {
+  return (
+    <Layout>
+      <BurgerBuilder />
+    </Layout>
+    );
+}
+
+export default App;
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+const INGREDIENT_PRICES = {
+    salad: 25,
+    cheese: 30,
+    meat: 70,
+    bacon: 50
+}
+
+class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            cheese: 0,
+            bacon: 0,
+            salad: 0,
+            meat: 0,
+        },
+        totalPrice: 150
+    }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updateCount = oldCount + 1;
+        const updateIngredients = {
+            ...this.state.ingredients
+        };
+        updateIngredients[type] = updateCount;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState({totalPrice: newPrice, ingredients: updateIngredients});
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Burger ingredients={this.state.ingredients} />
+                <BuildControls 
+                    ingredientAdded={this.addIngredientHandler}/>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
